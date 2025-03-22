642a07a1b8945d78a074e4d1dfee53cf976b1a2e
sarthaksahu03 <sarthaksahu333@gmail.com>
2025-03-22 12:08:20 +0530

    adding files

---
+++ src/hospital/management/system/Login.java
@@ -0,0 +1,14 @@
+package hospital.management.system;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+    Login(){
+        setSize(750,300);
+        setLayout(null);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Login();
+    }
+}
